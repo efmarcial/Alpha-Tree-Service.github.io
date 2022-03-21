6051440eb845a2cdeeb63fe32d825c44ac2dea0c
vilint <vilint@co.wake.nc.us>
2022-03-21 17:04:58 -0400

    added a foot and some bootstrap style

--- email.php
+++ email.php
@@ -59,14 +59,14 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#about-section">About<span class="sr-only">(current)</span></a>
+            <li class="nav-item ">
+              <a class="nav-link" href="index.html#about-section">About</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#contact-section">Contact</a>
+              <a class="nav-link" href="index.html#contact-section">Contact</a>
             </li>
 			<li class="nav-item">
-				<a class="nav-link" href="#services-section">Services</a>
+				<a class="nav-link" href="index.html#services-section">Services</a>
 			</li>
             
           </ul>
@@ -116,9 +116,9 @@
 
                                 
                                 echo "<h1>Thank you, Your Message has been sent</h1>";
-                                echo "See you in a bit";
+                                echo "<h4>We will be in contact with you soon</h4>";
                             } catch (Exception $e) {
-                                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                                echo "<h1>Message could not be sent. Mailer Error: {$mail->ErrorInfo}</h1>";
                                 
                             }
 
@@ -129,6 +129,31 @@
                 </div>        
             </div>
     </section>
+    <footer class="footer has-background-success">
+	<div class="content has-text-centered">
+		<p>
+			<a href="www.facebook.com">
+				<span class="fa-stack fa-2x">
+					<i class="fa-solid fa-circle fa-stack-2x"></i>
+					<i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+				</span>
+				
+			</a>
+
+			<a href="www.google.com">
+				<span class="fa-stack fa-2x">
+					<i class="fa-solid fa-circle fa-stack-2x"></i>
+					<i class="fa fa-google fa-stack-1x fa-inverse"></i>
+				</span>
+			</a>
+		</p>
+		<p class="has-text-white"><span>&COPY;</span>2022 All Rights Reserved </p>
+	</div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
     </body>
 </html>
 
