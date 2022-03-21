b2834fe5dda5906743a83bf1af26a10785cf9e8b
vilint <vilint@co.wake.nc.us>
2022-03-21 16:37:18 -0400

    trying to make this work

--- email.php
+++ email.php
@@ -1,3 +1,47 @@
+<?php
+                    //Import PHPMailer classes into the global namespace
+                    //These must be at the top of your script, not inside a function
+                    use PHPMailer\PHPMailer\PHPMailer;
+                    use PHPMailer\PHPMailer\SMTP;
+                    use PHPMailer\PHPMailer\Exception;
+
+                    //Load Composer's autoloader
+                    require 'vendor/autoload.php';
+
+                    //Create an instance; passing `true` enables exceptions
+                    $mail = new PHPMailer(true);
+
+                    try {
+                        //Server settings
+                        //$mail->SMTPDebug = 3;                      //Enable verbose debug output
+                        $mail->isSMTP();                                            //Send using SMTP
+                        $mail->Host       = 'ssl://smtp.gmail.com';                   //Set the SMTP server to send through
+                        $mail->SMTPSecure = "ssl";
+                        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                        $mail->Username   = 'drdfajardo18@gmail.com';                     //SMTP username
+                        $mail->Password   = 'Ef2083131';
+                        $mail->Port = 465;                                                  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+                        //Recipients
+                        $mail->From='drdfajardo18@gmail.com';
+                        $mail->addAddress('efmarcial1060@gmail.com');     //Add a recipient
+                                    //Name is optional
+
+                        //Content
+                        $mail->isHTML(true);                                  //Set email format to HTML
+                        $mail->Subject = 'Here is the subject';
+                        $mail->Body    = $_POST['message'];
+                        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+                        $mail->send();
+                        <h1>echo "Thank you, Your Message has been sent"</h1>
+                        <h4>We will be in touch with you soon.</h4>
+                    } catch (Exception $e) {
+                        <h1>echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";</h1>
+                        
+                    }
+
+                        ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -75,50 +119,7 @@ gtag('config', 'G-N9LS3TG3JF');
         <div class="container">
             <div class="columns">
                 <div class="column">
-                    <?php
-                    //Import PHPMailer classes into the global namespace
-                    //These must be at the top of your script, not inside a function
-                    use PHPMailer\PHPMailer\PHPMailer;
-                    use PHPMailer\PHPMailer\SMTP;
-                    use PHPMailer\PHPMailer\Exception;
-
-                    //Load Composer's autoloader
-                    require 'vendor/autoload.php';
-
-                    //Create an instance; passing `true` enables exceptions
-                    $mail = new PHPMailer(true);
-
-                    try {
-                        //Server settings
-                        //$mail->SMTPDebug = 3;                      //Enable verbose debug output
-                        $mail->isSMTP();                                            //Send using SMTP
-                        $mail->Host       = 'ssl://smtp.gmail.com';                   //Set the SMTP server to send through
-                        $mail->SMTPSecure = "ssl";
-                        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                        $mail->Username   = 'drdfajardo18@gmail.com';                     //SMTP username
-                        $mail->Password   = 'Ef2083131';
-                        $mail->Port = 465;                                                  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-                        //Recipients
-                        $mail->From='drdfajardo18@gmail.com';
-                        $mail->addAddress('efmarcial1060@gmail.com');     //Add a recipient
-                                    //Name is optional
-
-                        //Content
-                        $mail->isHTML(true);                                  //Set email format to HTML
-                        $mail->Subject = 'Here is the subject';
-                        $mail->Body    = $_POST['message'];
-                        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-                        $mail->send();
-                        <h1>echo "Thank you, Your Message has been sent"</h1>
-                        <h4>We will be in touch with you soon.</h4>
-                    } catch (Exception $e) {
-                        <h1>echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";</h1>
-                        
-                    }
-
-                        ?>
+                    
                     </div>
                 </div>
             </div>
