8d46e952c89d19a2b342488c5ae3e68037d43a21
vilint <vilint@co.wake.nc.us>
2022-03-21 17:55:32 -0400

    tess

--- email.php
+++ email.php
@@ -172,27 +172,6 @@
                 </div>        
             </div>
     </section>
-    <footer class="footer has-background-success">
-	<div class="content has-text-centered">
-		<p>
-			<a href="www.facebook.com">
-				<span class="fa-stack fa-2x">
-					<i class="fa-solid fa-circle fa-stack-2x"></i>
-					<i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-				</span>
-				
-			</a>
-
-			<a href="www.google.com">
-				<span class="fa-stack fa-2x">
-					<i class="fa-solid fa-circle fa-stack-2x"></i>
-					<i class="fa fa-google fa-stack-1x fa-inverse"></i>
-				</span>
-			</a>
-		</p>
-		<p class="has-text-white"><span>&COPY;</span>2022 All Rights Reserved </p>
-	</div>
-</footer>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
